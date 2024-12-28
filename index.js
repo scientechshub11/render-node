6164fa9b2862d95e5b4f2215f849197aea82e2d3
scientechshub11 <56425994+scientechshub11@users.noreply.github.com>
2024-12-29 01:08:32 +0530

    added changes

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app =express();
 require('dotenv').config();
 const port = process.env.port || 3900
+
+app.get('/', (req, res)=>{
+    res.json({
+        status:200,
+        message:"code deployed"
+    })
+})
+
 app.listen(port, ()=>{
     console.log(`app listen to the port ${port}`)
 })
